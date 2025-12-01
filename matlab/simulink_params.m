d3d3369a17c7e74546800c40a46074fa83a63bc5
George Hany <georgehany064@gmail.com>
2025-12-01 16:56:48 +0100

    adding simulink files of the half, full, center tapped rectifiers with parameters file but only with R load

---
+++ matlab/simulink_params.m
@@ -0,0 +1,14 @@
+f = 50; % Hz
+alpha_deg = 30; % To change the firing angle you only need to change this value
+alpha_2_deg = 180 + alpha_deg;
+Vin_rms = 230; % Volt
+Vin_peak = sqrt(2)*Vin_rms;
+
+alpha_rad = (alpha_deg*pi)/(180.0);
+alpha_2_rad = (alpha_2_deg*pi)/(180.0);
+
+alpha_1_t = alpha_rad/(2*pi*f);
+alpha_2_t = alpha_2_rad/(2*pi*f);
+
+R = 20; % Ohm
+L = 0.1; % Henry
